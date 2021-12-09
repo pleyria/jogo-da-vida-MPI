64ecf7fdf37723e037220f1854eda16f2c8dcd89
pleyria <pedroleyria@gmail.com>
2021-12-09 17:25:09 -0300

    commit

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Jogo da Vida MPI - Atividade de Programação Concorrente e Distribuída
 
 ## Descrição
+Implementação de uma versão paralela do jogo da vida em linguagem C, utilizando a biblioteca MPI para execução com multiplos processos de memória distribuída.
+
+É possível escolher o número de processos a serem criados no programa. Os múltiplos processos recebem uma parte do domínio do problema a ser resolvido e trocam mensagens entre si para produzir o resultado global.
 
 ## Estrutura dos Arquivos
 O arquivo `jogo-da-vida.c` contém o código da versão paralela do programa do jogo da vida, desenvolvido com a biblioteca MPI.
@@ -8,10 +11,12 @@ O arquivo `jogo-da-vida.c` contém o código da versão paralela do programa do 
 É necessário ter o pacote `mpich` instalado.
 > No Ubuntu/Linux Mint: $ sudo apt-get install mpich
 
-Para compilar o código usa-se o comando `mpicc jogo-da-vida.c -o jogo-da-vida`.
+Para compilar o código usa-se o comando `mpicc -O3 jogo-da-vida.c -o jogo-da-vida`.
 
 Para executar o programa compilado, usa-se `mpiexec -np 4 ./jogo-da-vida`. 
 > -np: número de processos
+
+O número de processos sempre deve ser um a mais do que o número esperado para executar a simulção. Isso porque um dos processos não participa do processamento do jogo da vida e serve apenas para agregar os resultados parciais produzidos pelos outros processos. Portanto, para executar o programa como, por exemplo, 4 processos em paralelo, é preciso usar o argumento `-np 5`, onde são empregados 5 processos: 4 deles para executar a simulação do jogo da vida e 1 para reunir e exibir os resultados.
 
 ## Resultados
 
--- jogo-da-vida.c
+++ jogo-da-vida.c
@@ -9,6 +9,47 @@
 #define ALIVE 1
 #define DEAD 0
 
+/* Recebe um subgrid (size+2)xN.
+ * Insere uma célula viva na posicao (i, j) se i estiver entre li e lf. */
+void insertCell(int** grid, int i, int j, int li, int lf){
+	if(i >= li && i <= lf)
+		grid[i - li][j] = ALIVE;
+}
+
+/* Recebe um subgrid (size+2)xN.
+ * Insere um glider a partir da posicao (0, 0) se ela existir no subgrid.
+ * Insere um R-pentomino a partir da posicao (10, 30) se ela existir no subgrid. 
+ * O intervalo FECHADO de linhas existentes no subgrid é dado por li e lf. */
+void startGrid(int** grid, int size, int N, int li, int lf){
+	int i, j;
+	int lin, col;
+
+	// linha 0 			-> guarda vizinho de cima
+	// linha 1...size 	-> conteudo da subtabela
+	// linha size+1 	-> guardda vizinho de baixo
+
+	// Inicializa com zeros
+	for(i=1; i <= size; i++)
+		for(j=0; j<N; j++)
+			grid[i][j] = DEAD;
+
+	//GLIDER
+	lin = 1, col = 1;
+	insertCell(grid, lin  , col+1, li, lf);
+	insertCell(grid, lin+1, col+2, li, lf);
+	insertCell(grid, lin+2, col  , li, lf);
+	insertCell(grid, lin+2, col+1, li, lf);
+	insertCell(grid, lin+2, col+2, li, lf);
+
+	//R-pentomino
+	lin =10; col = 30;
+	insertCell(grid, lin  , col+1, li, lf);
+	insertCell(grid, lin  , col+2, li, lf);
+	insertCell(grid, lin+1, col  , li, lf);
+	insertCell(grid, lin+1, col+1, li, lf);
+	insertCell(grid, lin+2, col+1, li, lf);
+}
+
 int getNeighbors(int** grid, int i, int j, int N){
 	int up, down, left, right, count;
 
@@ -61,6 +102,17 @@ void simulate(int** grid, int** newgrid, int size, int N){
 	}
 }
 
+int countAlive(int** grid, int size, int N){
+	int i, j, count;
+
+	count = 0;
+
+	for(i = 1; i <= size; i++)
+		for(j = 0; j < N; j++)
+			count += grid[i][j];
+
+	return count;
+}
 
 int main(int argc, char** argv){
 	int i, j;
@@ -75,13 +127,20 @@ int main(int argc, char** argv){
 	int li, lf, size;
 	// estruturas do grid
 	int **grid, **newgrid;
-
+	// id do processo mestre
+	int master;
+	// contagem de celulas vivas
+	int soma, alive;
+	// contagem de tempo
+	double start, finish;
+	// variaveis de controle do MPI
+	MPI_Request req;
 	MPI_Status status;
 
 	// Tabuleiro 2048x2048
-	N = 12;
+	N = 2048;
 	// 2000 geracoes
-	G = 1;
+	G = 2000;
 
 	// Inscreve os processos na compuitacao do MPI
 	MPI_Init(&argc, &argv);
@@ -89,101 +148,142 @@ int main(int argc, char** argv){
 	// obtem numero total de processos e
 	// identificador do processo
 	MPI_Comm_size(MPI_COMM_WORLD, &numProc);
+	master = numProc - 1;
+	numProc--;
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+	// o processo com o rank mais alto nao calcula o jogo da vida
+	// e serve apenas para reunir os resultados
 
-	// divisao do tabuleiro por processos
-	size = N/numProc;		// numero de linhas por processo
-	li = pid*size;			// inicio do intervalo
-	lf = li + size - 1;		// fim do intervalo
-	if(pid == numProc - 1){	// ultimo processo pega o resto
-		lf += N % numProc;
-		size += N % numProc;
+	if(pid < master){
+		// divisao do tabuleiro por processos
+		size = N/numProc;		// numero de linhas por processo
+		li = pid*size;			// inicio do intervalo
+		lf = li + size - 1;		// fim do intervalo
+		if(pid == numProc - 1){	// ultimo processo pega o resto
+			lf += N % numProc;
+			size += N % numProc;
+		}
+
+		// matrizes <size+2> linhas e <N> colunas
+		// aloca duas linhas a mais para receber valores dos vizinhos (de cima e de baixo)
+		// ----
+		// linha 0:			vizinho de cima
+		// linhas 1...size:	parte atual
+		// linha size+1:	vizinho de baixo
+		// ----
+		grid = (int**) malloc((size+2) * sizeof(int*));		// aloca linhas
+		newgrid = (int**) malloc((size+2) * sizeof(int*));
+		for(i = 0; i < size+2; i++){						// aloca colunas
+			grid[i] = (int*) malloc(N * sizeof(int));
+			newgrid[i] = (int*) malloc(N * sizeof(int));
+		}
+
+		// inicializa o grid
+		startGrid(grid, size, N, li, lf);
 	}
 
-	// matrizes <size+2> linhas e <N> colunas
-	// aloca duas linhas a mais para receber valores dos vizinhos (de cima e de baixo)
-	// ----
-	// linha 0:			vizinho de cima
-	// linhas 1...size:	parte atual
-	// linha size+1:	vizinho de baixo
-	// ----
-	grid = (int**) malloc((size+2) * sizeof(int*));		// aloca linhas
-	newgrid = (int**) malloc(size * sizeof(int*));
-	for(i = 0; i < size+2; i++){						// aloca colunas
-		grid[i] = (int*) malloc(N * sizeof(int));
-		newgrid[i] = (int*) malloc(N * sizeof(int));
-	}
-
-	// TESTE
-	printf("[%d/%d]\trange(%d | %d)\n", pid, numProc, li, lf);
-
-	// espera todos os processos criarem as matrizes
+	// espera todos os processos criarem e inicializarem as matrizes
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	// precisa criar a rotina de inicializacao
+	// inicia a contagem do tempo
+	if(pid == master)
+		start = MPI_Wtime();
 
 	// Realiza a simulacao para G geracoes
 	// Alterna a posicao de grid e newgrid nas chamadas
 	// (G impar -> resposta final em newgrid)
 	// (G par -> resposta final em grid)
 	for(i = 0; i < G; i++){
-		// processos vizinhos compartilham informacoes das bordas
+		if(pid < master){
+			// processos vizinhos compartilham informacoes das bordas
+			// alterna entre grid e newgrid
+			if(i % 2 == 0){
+				// envia borda inferior do grid para o vizinho de baixo (tag = 2)
+				MPI_Isend(grid[size], N, MPI_INT, (pid+1)%numProc, 2, MPI_COMM_WORLD, &req);
+				// envia borda superior do grid para o vizinho de cima (tag = 3)
+				if(pid == 0)
+					MPI_Isend(grid[1], N, MPI_INT, numProc-1, 3, MPI_COMM_WORLD, &req);
+				else
+					MPI_Isend(grid[1], N, MPI_INT, pid-1, 3, MPI_COMM_WORLD, &req);
 
-		// alterna entre grid e newgrid
-		if(i % 2 == 0){
-			// envia borda inferior do grid para o vizinho de baixo (tag = 2)
-			MPI_Send(grid[size], N, MPI_INT, (pid+1)%numProc, 2, MPI_COMM_WORLD);
-			// envia borda superior do grid para o vizinho de cima (tag = 3)
-			if(pid == 0)
-				MPI_Send(grid[1], N, MPI_INT, numProc-1, 3, MPI_COMM_WORLD);
-			else
-				MPI_Send(grid[1], N, MPI_INT, pid-1, 3, MPI_COMM_WORLD);
+				// recebe borda inferior do grid vizinho de cima (tag = 2)
+				if(pid == 0)
+					MPI_Recv(grid[0], N, MPI_INT, numProc-1, 2, MPI_COMM_WORLD, &status);
+				else
+					MPI_Recv(grid[0], N, MPI_INT, pid-1, 2, MPI_COMM_WORLD, &status);
+				// recebe borda superior do vizinho de baixo (tag = 3)
+				MPI_Recv(grid[size+1], N, MPI_INT, (pid+1)%numProc, 3, MPI_COMM_WORLD, &status);
 
+				// calcula a proxima geracao
+				simulate(grid, newgrid, size, N);
 
-			// recebe borda inferior do grid vizinho de cima (tag = 2)
-			if(pid == 0)
-				MPI_Recv(grid[0], N, MPI_INT, numProc-1, 2, MPI_COMM_WORLD, &status);
-			else
-				MPI_Recv(grid[0], N, MPI_INT, pid-1, 2, MPI_COMM_WORLD, &status);
-			// recebe borda superior do vizinho de baixo (tag = 3)
-			MPI_Recv(grid[size+1], N, MPI_INT, (pid+1)%numProc, 3, MPI_COMM_WORLD, &status);
+				// conta as celulas vivas na subtabela
+				alive = countAlive(grid, size, N);
+			}
+			else {
+				// envia borda inferior do grid para o vizinho de baixo (tag = 2)
+				MPI_Isend(newgrid[size], N, MPI_INT, (pid+1)%numProc, 2, MPI_COMM_WORLD, &req);
+				// envia borda superior do grid para o vizinho de cima (tag = 3)
+				if(pid == 0)
+					MPI_Isend(newgrid[1], N, MPI_INT, numProc-1, 3, MPI_COMM_WORLD, &req);
+				else
+					MPI_Isend(newgrid[1], N, MPI_INT, pid-1, 3, MPI_COMM_WORLD, &req);
 
-			// calcula a proxima geracao
-			simulate(grid, newgrid, size, N);
-		}
-		else {
-			// envia borda inferior do grid para o vizinho de baixo (tag = 2)
-			MPI_Send(newgrid[size], N, MPI_INT, (pid+1)%numProc, 2, MPI_COMM_WORLD);
-			// envia borda superior do grid para o vizinho de cima (tag = 3)
-			if(pid == 0)
-				MPI_Send(newgrid[1], N, MPI_INT, numProc-1, 3, MPI_COMM_WORLD);
-			else
-				MPI_Send(newgrid[1], N, MPI_INT, pid-1, 3, MPI_COMM_WORLD);
+				// recebe borda inferior do grid vizinho de cima (tag = 2)
+				if(pid == 0)
+					MPI_Recv(newgrid[0], N, MPI_INT, numProc-1, 2, MPI_COMM_WORLD, &status);
+				else
+					MPI_Recv(newgrid[0], N, MPI_INT, pid-1, 2, MPI_COMM_WORLD, &status);
+				// recebe borda superior do vizinho de baixo (tag = 3)
+				MPI_Recv(newgrid[size+1], N, MPI_INT, (pid+1)%numProc, 3, MPI_COMM_WORLD, &status);
 
+				// calcula a proxima geracao
+				simulate(newgrid, grid, size, N);
 
-			// recebe borda inferior do grid vizinho de cima (tag = 2)
-			if(pid == 0)
-				MPI_Recv(newgrid[0], N, MPI_INT, numProc-1, 2, MPI_COMM_WORLD, &status);
-			else
-				MPI_Recv(newgrid[0], N, MPI_INT, pid-1, 2, MPI_COMM_WORLD, &status);
-			// recebe borda superior do vizinho de baixo (tag = 3)
-			MPI_Recv(newgrid[size+1], N, MPI_INT, (pid+1)%numProc, 3, MPI_COMM_WORLD, &status);
-
-			// calcula a proxima geracao
-			simulate(newgrid, grid, size, N);
+				// conta as celulas vivas na subtabela
+				alive = countAlive(newgrid, size, N);
+			}
 		}
 
 		// espera todos os processos terminarem o calculo
 		MPI_Barrier(MPI_COMM_WORLD);
+
+		if(pid < master){
+			// envia o numero de vivos para o mestre (tag = 4)
+			MPI_Isend(&alive, 1, MPI_INT, master, 4, MPI_COMM_WORLD, &req);
+		}
+		else{
+			// mestre recebe e conta (tag = 4)
+			soma = 0;
+			for(j = 0; j <= master - 1; j ++){
+				MPI_Recv(&alive, 1, MPI_INT, j, 4, MPI_COMM_WORLD, &status);
+				soma += alive;
+			}
+			printf("Geracao [%d]\t %d celulas vivas\n", i, soma);
+		}
+
+		// espera todos os processos terminarem a contagem
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
-	// libera as matrizes
-	for(i = 0; i < size+ 2 ; i++){
-		free(grid[i]);
-		free(newgrid[i]);
+	// termina a contagem do tempo
+	if(pid == master)
+		finish = MPI_Wtime();
+
+	if(pid < master){
+		// libera as matrizes
+		for(i = 0; i < size + 2 ; i++){
+			free(grid[i]);
+			free(newgrid[i]);
+		}
+		free(grid);
+		free(newgrid);
+		grid = NULL;
+		newgrid = NULL;
 	}
-	free(grid);
-	free(newgrid);
+
+	if(pid == master)
+		printf("\nTempo total = %.14lf segundos.\n", finish-start);
 
 	// Finaliza os processos
 	MPI_Finalize();
